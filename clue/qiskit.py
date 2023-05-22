784f6bff9437f2ed7717dbe7569a94da59d8cb1d
Antonio Jimenez-Pastor <kethewal@gmail.com>
2023-05-22 13:49:48 +0200

    Adding main gates and definitions to work with the case studies for quantum paper

--- clue/qiskit.py
+++ clue/qiskit.py
@@ -7,9 +7,9 @@ r'''
     corresponding unitary matrices.
 '''
 
-from itertools import product
 from qiskit import execute, QuantumCircuit, Aer
-from numpy import asarray
+from numpy import array, matmul, block, cdouble, asarray, sqrt, kron, eye, ones, zeros
+from numpy.random import rand
 from sympy import CC
 
 from .clue import FODESystem
@@ -36,3 +36,42 @@ class DS_QuantumCircuit(FODESystem):
             lumping_subspace=kwds.pop("lumping_subspace", NumericalSubspace), 
             lumping_subspace_kwds=kwds
         )
+
+###########################################################################
+### Building circuit matrices from diagrams
+###
+### In this part of the file we provide several methods that allow to take 
+### numpy matrices that describes part of a quantum circuit and recombines
+### them to get a final quantum circuit described with its unitary matrix.
+###########################################################################
+Hadamard = (1/sqrt(2))*array([[1,1],[1,-1]], dtype=cdouble)
+PauliX = array([[0,1],[1,0]], dtype=cdouble); qbit_plus = PauliX
+PauliY = array([[0,-1j],[1j,0]], dtype=cdouble)
+PauliZ = array([[1,0],[0,-1]], dtype=cdouble)
+Phase = array([[1,0],[0,1j]], dtype=cdouble)
+
+numerical_threshold = 1e-10
+
+def ControlledGate(U):
+    size = U.shape[0]
+    return block([[eye(size), zeros(size)], [zeros(size), U]])
+
+def Entangle(*U):
+    return kron(*U)
+
+def inner_product(v, w):
+    return matmul(v, w.transpose().conjugate())
+
+def measure(v):
+    if len(v.shape) != 1:
+        raise TypeError("Only a state can be measured")
+    if inner_product(v,v) - 1 > numerical_threshold:
+        raise ValueError("The state is not well defined") 
+    
+    val = rand()
+    val -= v[0]*v[0].conjugate(); i = 0
+    while val > 0:
+        i += 1
+        val -= v[i]*v[i].conjugate()
+
+    return i
