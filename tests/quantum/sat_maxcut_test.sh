b1fc3d8e6f7a0b3d694d0d09eaf636ea7e316330
Antonio Jimenez-Pastor <kethewal@gmail.com>
2023-09-21 16:46:43 +0200

    Fixed script

--- tests/quantum/sat_maxcut_test.sh
+++ tests/quantum/sat_maxcut_test.sh
@@ -1,10 +1,13 @@
-## Test for Q_SAT
+## Faster tests
+python3 q_sat.py -M 8 -r 20;
+python3 q_maxcut.py -M 8 -r 20;
 python3 q_sat.py -M 10 -r 20 -t ddsim;
+python3 q_maxcut.py -M 10 -r 20 -t ddsim;
+## Test for Q_SAT
 python3 q_sat.py -m 11 -M 20 -r 10 -t ddsim;
 python3 q_sat.py -m 21 -M 25 -r 5 -t ddsim;
 python3 q_sat.py -r 20;
-## Test for Q_SAT
-python3 q_maxcut.py -M 10 -r 20 -t ddsim;
+## Test for MAXCUT
 python3 q_maxcut.py -m 11 -M 20 -r 10 -t ddsim;
 python3 q_maxcut.py -m 21 -M 25 -r 5 -t ddsim;
 python3 q_maxcut.py -r 20;
