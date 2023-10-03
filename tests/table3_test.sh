ac43ccad9d2b60c05bff364c1d8b4c31c61e56d4
Antonio Jimenez-Pastor <kethewal@gmail.com>
2023-10-03 11:59:30 +0200

    Added test to be run for Table 3

---
+++ tests/table3_test.sh
@@ -0,0 +1,4 @@
+python3 q_maxcut.py -t full_ddsim -m 5 -M 9 -r 50;
+python3 q_sat.py -t full_ddsim -m 5 -M 9 -r 50;
+python3 q_maxcut.py -t full_clue -m 5 -M 9 -r 50;
+python3 q_sat.py -t full_clue -m 5 -M 9 -r 50;
