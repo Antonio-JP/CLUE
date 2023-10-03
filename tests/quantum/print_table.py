553cc36bab16dd1a3f22bf6d0a693a0c3b18d8e7
Antonio Jimenez-Pastor <kethewal@gmail.com>
2023-10-03 16:14:03 +0200

    Removed also memory outliers

--- tests/quantum/print_table.py
+++ tests/quantum/print_table.py
@@ -11,8 +11,16 @@ if __name__ == "__main__":
         grouped = data.groupby(by=["size"] + (["kappa"] if "kappa" in data.columns else []))
         q_low = grouped["time_lumping"].quantile(0.05)
         q_hi  = grouped["time_lumping"].quantile(0.95)
+        M_low = grouped["memory (MB)"].quantile(0.05)
+        M_high = grouped["memory (MB)"].quantile(0.95)
         get_filter = (lambda D, r : D[r["size"]]) if (not "kappa" in data.columns) else (lambda D, r : D[r["size"],r["kappa"]])
-        data = pd.DataFrame([row for (i,row) in data.iterrows() if (row["time_lumping"] < get_filter(q_hi, row) and row["time_lumping"] > get_filter(q_low,row))])
+        data = pd.DataFrame(
+            [row for (_,row) in data.iterrows() if (
+                row["time_lumping"] < get_filter(q_hi, row) and 
+                row["time_lumping"] > get_filter(q_low,row) and
+                row["memory (MB)"] < get_filter(M_high, row) and 
+                row["memory (MB)"] > get_filter(M_low, row)
+            )])
     
     ## PRINTING RESULTING DATA
     print(data.drop(columns=data.columns[-1:]).groupby(by=["size"] + (["kappa"] if "kappa" in data.columns else [])).mean())
